33641fcaf5313feef8873d0547c00f2f8d07f352
kevin1213877 <j.xiao@i4ai.org>
2024-04-11 11:56:07 +0800

    update nsga2

--- code/ea_supernet.py
+++ code/ea_supernet.py
@@ -9,23 +9,37 @@ import scipy.signal as signal
 import numpy as np
 from modules import ComplexConv, LUT1D
 from utils import (data_process, freq_shift, optimizer_init, data_load, cal_power, get_performance)
+
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.core.evaluator import Evaluator
+from pymoo.core.problem import Problem
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.repair.rounding import RoundingRepair
+from pymoo.operators.sampling.rnd import IntegerRandomSampling
+from pymoo.problems.static import StaticProblem
+from pymoo.visualization.scatter import Scatter
+from pymoo.config import Config
+
+Config.warnings['not_compiled'] = False
+
 # get current project path
 project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 parser = argparse.ArgumentParser("pimc")
-parser.add_argument('--addr', type=str, default= project_path + '/data/')
+parser.add_argument('--addr', type=str, default=project_path + '/data/')
 parser.add_argument('--name', type=str, default='pim_16t_221110_38dBm_fr4_rnd32_1.pth')
 parser.add_argument('--lr', type=float, default=1e-2, help='init learning rate')
 parser.add_argument('--step', type=int, default=50, help='decay step')
 parser.add_argument('--lr_gamma', type=float, default=0.5, help='decay rate')
 parser.add_argument('--epochs', type=int, default=1, help='num of training epochs')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
-parser.add_argument('--pop_size', type=int, default=20)
-parser.add_argument('--mut_rate', type=float, default=0.8)
+# parser.add_argument('--pop_size', type=int, default=20)
+# parser.add_argument('--mut_rate', type=float, default=0.8)
 parser.add_argument('--generation', type=int, default=100)
 parser.add_argument('--device', type=str, default='cpu')
 parser.add_argument('--bs', type=int, default=256, help='batch size')
-parser.add_argument('--bn', type=int, default=100, help='batch number')
+parser.add_argument('--bn', type=int, default=20, help='batch number')
 parser.add_argument('--tr', type=int, default=0.8, help='train ratio')
 parser.add_argument('--chnl', type=int, default=16, help='channel number')
 
@@ -43,7 +57,8 @@ torch.cuda.manual_seed(args.seed)
 
 logging.info(f"args: {args}")
 
-flt = freq_shift(signal.firwin(255, [50 * 1e6], fs=245.76e6),  0, 245.76)
+flt = freq_shift(signal.firwin(255, [50 * 1e6], fs=245.76e6), 0, 245.76)
+
 
 class Block(nn.Module):
     def __init__(self, arg):
@@ -112,10 +127,14 @@ def infer(valid_queue, model, scale):
             err_all.permute(1, 0, 2).cpu().detach().numpy() * scale)
 
 
-def main(argv):
+def supernet(pop):
     tx, rx, nf, txn, rxn, scale = data_process(args)
-    model = Model(argv).to(args.device)
-    optimizer, scheduler = optimizer_init(model, pop_size, args)
+    model = Model(pop).to(args.device)
+    param_num = np.zeros((pop.shape[0], 1))
+    for i in range(pop.shape[0]):
+        param_num[i, 0] = sum(p.numel() for p in model.blocks[i].parameters())
+
+    optimizer, scheduler = optimizer_init(model, pop.shape[0], args)
     for epoch in range(args.epochs):
         tra_all_dl, tra_dl, val_dl, L0 = data_load(txn, rxn, args)
         start = time.time()
@@ -124,8 +143,8 @@ def main(argv):
         y_val, e_val = infer(val_dl, model, scale)
         end = time.time()
 
-        ape_all = np.zeros((pop_size, 1))
-        for i in range(pop_size):
+        ape_all = np.zeros((pop.shape[0], 1))
+        for i in range(pop.shape[0]):
             pim_val, res_val, per_val, ape_val = get_performance(cal_power(rx[:, L0:], args.chnl)[0],
                                                                  cal_power(nf[:, L0:], args.chnl)[0],
                                                                  cal_power(e_val[:, :, i], args.chnl)[0],
@@ -133,73 +152,55 @@ def main(argv):
             ape_all[i, 0] = ape_val
         k = np.argmax(ape_all[:, 0])
         print('epoch:', epoch, 'cost time:', round(end - start, 2),
-              'pim:', pim_val, 'max_ape:', ape_all[k, 0], 'best arg:', argv[k])
-    return ape_all
+              'pim:', pim_val, 'max_ape:', ape_all[k, 0], 'best arg:', pop[k])
+    return np.hstack((-ape_all, param_num))
 
 
-def selection(pop, fit):
-    num = pop.shape[0]
-    child = np.zeros_like(pop)
-    for i in range(child.shape[0]):
-        candidates = np.random.choice(num, 3, replace=False)
-        winner = np.argmin(fit[candidates, 0])
-        child[i, :] = pop[candidates[winner], :]
-    return child
+def ea():
+    problem = Problem(n_var=5, n_obj=2, vtype=int, xl=np.ones(5), xu=np.array([4, 49, 4, 51, 20]))
+
+    # create the algorithm object
+    algorithm = NSGA2(pop_size=20,
+                      sampling=IntegerRandomSampling(),
+                      crossover=SBX(prob=1.0, eta=3.0, vtype=float, repair=RoundingRepair()),
+                      mutation=PM(prob=1.0, eta=3.0, vtype=float, repair=RoundingRepair()),
+                      eliminate_duplicates=True)
+
+    # create an algorithm object that never terminates
+    algorithm.setup(problem)
+
+    # until the algorithm has no terminated
+    for n_gen in range(args.generation):
+        # ask the algorithm for the next solution to be evaluated
+        pop = algorithm.ask()
+
+        # get the design space values of the algorithm
+        X = pop.get("X")
+
+        # implement your evluation
+        F = supernet(X)
+
+        static = StaticProblem(problem, F=F)
+        Evaluator().eval(static, pop)
+
+        # returned the evaluated individuals which have been evaluated or even modified
+        algorithm.tell(infills=pop)
+
+        # obtain the result objective from the algorithm
+        res = algorithm.result()
+
+        logging.info(
+            f'---------------generation:{n_gen}------------------\n'
+            f'----------------------res.X:-----------------------\n'
+            f'{res.X}\n'
+            f'----------------------res.F:-----------------------\n'
+            f'{np.array2string(res.F, formatter={"float_kind": lambda x: "%.2f" % x})}\n')
+
+    # calculate a hash to show that all executions end with the same result
+    plot = Scatter()
+    plot.add(res.F, facecolor="none", edgecolor="red")
+    plot.show()
 
 
-def crossover(pop):
-    num = pop.shape[0]
-    child = np.zeros_like(pop)
-    for i in range(child.shape[0]):
-        r1, r2 = np.random.choice(num, 2, replace=False)
-        for j in range(child.shape[1]):
-            if np.random.rand() < 0.5:
-                child[i, j] = pop[r1, j]
-            else:
-                child[i, j] = pop[r2, j]
-    return child
-
-
-def mutate(pop):
-    num = pop.shape[0]
-    child = np.zeros_like(pop)
-    for i in range(child.shape[0]):
-        r1 = np.random.choice(num, 1)[0]
-        for j in range(child.shape[1]):
-            if np.random.rand() < args.mut_rate:
-                if j == 0 or j == 2:
-                    child[i, j] = np.random.randint(1, 4)
-                elif j == 1:
-                    child[i, j] = np.random.randint(1, 49)
-                elif j == 3:
-                    child[i, j] = np.random.randint(21) * 2 + 1
-                elif j == 4:
-                    child[i, j] = np.random.randint(1, 17)
-            else:
-                child[i, j] = pop[r1, j]
-    return child
-
-
-if __name__ == "__main__":
-    pop_size = args.pop_size
-    inchnl = np.random.randint(1, 4, size=(pop_size, 1))
-    branch = np.random.randint(1, 49, size=(pop_size, 1))
-    outchnl = np.random.randint(1, 4, size=(pop_size, 1))
-    kernel = np.random.randint(21, size=(pop_size, 1)) * 2 + 1
-    nspline = np.random.randint(1, 17, size=(pop_size, 1))
-    argv = np.hstack((inchnl, branch, outchnl, kernel, nspline))
-
-    fit = main(argv)
-
-    for gen in range(args.generation):
-        child = selection(argv, fit)
-        child = crossover(child)
-        child = mutate(child)
-        fit_child = main(child)
-
-        pop_pool = np.concatenate([argv, child], axis=0)
-        fit_pool = np.concatenate([fit, fit_child], axis=0)
-        selected = np.argsort(-fit_pool[:, 0])[:pop_size]
-        argv = pop_pool[selected, :]
-        fit = fit_pool[selected, :]
-        logging.info(f'gen: {gen}, ape: {fit[0, 0]}, argv: {argv[0]}')
+if __name__ == '__main__':
+    ea()
